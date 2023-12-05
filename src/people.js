6615fc88949ad8b5aca5fbb80655810236ff1d04
max <mcnic@users.noreply.github.com>
2023-12-05 00:11:42 -0800

    refactoring list

--- src/people.js
+++ src/people.js
@@ -3,7 +3,6 @@ const apiUrl = 'https://www.swapi.tech/api/'
 const getPeople = async () => {
   const resp = await fetch(apiUrl + 'people');
   const people = await resp.json();
-  console.log(people);
 
   if (people.message !== "ok") {
     const fragment = document.createElement("h1");
@@ -14,16 +13,15 @@ const getPeople = async () => {
   const ul = document.createElement("ul");
 
   for (el of people.results) {
-    console.log(el);
     const li = document.createElement("li");
-    li.appendChild(hero(el))
+    li.appendChild(Hero(el))
     ul.appendChild(li)
   }
 
   return ul
 }
 
-const hero = (val) => {
+const Hero = (val) => {
   const { uid, name } = val || {}
   const hero = document.createElement("a");
   hero.className = 'hero'
@@ -31,20 +29,4 @@ const hero = (val) => {
   hero.setAttribute('href', 'hero.html?id=' + uid)
   hero.textContent = name
   return hero
-}
-
-const getHero = async () => {
-  const resp = await fetch(apiUrl + 'people/1');
-  const hero = await resp.json();
-  console.log(hero);
-
-  if (hero.message !== "ok") {
-    const fragment = document.createElement("h1");
-    fragment.textContent = 'error ' + hero.message;
-    return fragment
-  }
-
-  const head = document.createElement("h1");
-  head.textContent = 'Hero'
-  return head
 }
